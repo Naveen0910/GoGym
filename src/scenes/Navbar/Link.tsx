f2f67d709bced163fbcf83fbbfc8f7ebc1bb2537
Naveen0910 <125568964+Naveen0910@users.noreply.github.com>
2024-01-29 20:28:46 +0530

    NavbarDone

--- src/scenes/Navbar/Link.tsx
+++ src/scenes/Navbar/Link.tsx
@@ -1,13 +1,15 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
-
+import { SelectedPage } from "@/shared/types";
 type Props = {
   page: string;
-  selectedPage: string;
-  setSelectedPage: (value: string) => void;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
-  const lowerCasePage: string = page.toLocaleLowerCase().replace(/ /g, "");
+  const lowerCasePage = page
+    .toLocaleLowerCase()
+    .replace(/ /g, "") as SelectedPage;
   return (
     <AnchorLink
       className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""} 
