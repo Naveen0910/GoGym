42571e1f3f7b85c25fac11efbca0a51779dbf0d4
Naveen0910 <125568964+Naveen0910@users.noreply.github.com>
2024-02-01 14:35:06 +0530

    bug fixes

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,4 +1,4 @@
-import Logo from "@/assets/Logo.png";
+import Logo from "@/assets/dp.jpg";
 
 const Footer = () => {
   return (
@@ -7,22 +7,28 @@ const Footer = () => {
         <div className="mt-16 basis-1/2 md:mt-0">
           <img alt="logo" src={Logo} />
           <p className="my-5">
-            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
-            purus et arcu massa dictum condimentum. Augue scelerisque iaculis
-            orci ut habitant laoreet. Iaculis tristique.
+            As a former Mechanical Engineer who has transitioned into the field
+            of web development, I bring a unique perspective to the world of
+            technology. Proficient in an array of technologies including
+            React.js, Node.js, Express, MongoDB, JavaScript, Python, HTML, CSS,
+            and MySQL, I possess a versatile skill set that enables me to excel
+            in both frontend and backend development. My passion lies in
+            harnessing technology to tackle intricate challenges, offering
+            innovative solutions to complex problems
           </p>
-          <p>© Evogym All Rights Reserved.</p>
+          <p>© Naveen All Rights Reserved.</p>
         </div>
-        <div className="mt-16 basis-1/4 md:mt-0">
+        {/* <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
           <p className="my-5">Massa orci senectus</p>
           <p className="my-5">Et gravida id et etiam</p>
           <p>Ullamcorper vivamus</p>
-        </div>
+        </div> */}
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
-          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
-          <p>(333)425-6825</p>
+          <p className="my-5">Naveen</p>
+          <p className="my-5">Naveen850850@gmail.com</p>
+          <p>9133430919</p>
         </div>
       </div>
     </footer>
